655223074221c866675bf8d5c1df4ab27115c4fe
harvish <harvish@gmail.com>
2013-07-06 18:03:45 +0530

    FUll Project

---
+++ centroid/centroid.ino
@@ -0,0 +1,128 @@
+#include "pt.h"
+#include <Stepper.h>
+
+#define STEPS 100
+Stepper stepperX(STEPS, 46, 47, 48, 49);
+Stepper stepperY(STEPS, 50, 51, 52, 53);
+
+int n=5; //number of pin diodes in a row or column
+int dist=1; // distance between two pin diode
+
+float X,Y;
+float row[6][2],col[6][2],rowsum=0.0,colsum=0.0;
+int nrow=0,ncol=0,wait;
+static struct pt snr, mtr;
+
+void setup()
+{
+  int tmp;
+  PT_INIT(&snr);
+  PT_INIT(&mtr);
+  
+  //output to motor
+  for(tmp=0;tmp++;tmp<8)
+    pinMode(46+tmp, OUTPUT);
+  
+  Serial.begin(9600);
+
+  // set the speed of the motor to 30 RPMs
+  stepperX.setSpeed(30);
+  stepperY.setSpeed(30);
+}
+
+static int motor(struct pt *pt)
+{
+  PT_BEGIN(pt);
+  while(1)
+  {
+    PT_WAIT_UNTIL(pt, protothread1_flag != 0);      
+    stepperX.step(X);  
+    stepperY.step(Y);  
+  }
+  PT_END(pt);
+}
+
+static int sensor(struct pt *pt)
+{
+  PT_BEGIN(pt);
+  while(1)
+  {
+    PT_WAIT_UNTIL(pt, protothread1_flag != 0);
+
+    int a[][6]=  {
+      {
+        1,1,1,0,0,0      }
+      ,
+      {
+        1,1,1,0,0,0      }
+      ,
+      {
+        1,1,1,0,0,0      }
+      ,
+      {
+        0,0,0,0,0,0      }
+      ,
+      {
+        0,0,0,0,0,0      }
+      ,
+      {
+        0,0,0,0,0,0      }
+    };
+
+
+    //Initialize the array
+    for(int i=0;i<n;i++)
+      row[i][0]=row[i][1]=col[i][0]=col[i][1]=0;
+    rowsum=colsum=0.0;
+    nrow=ncol=0;
+
+    //Calculate number of 1s in row and column
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+      {
+        if(a[i][j])
+        {
+          row[i][0]+=j;
+          row[i][1]++;
+          col[j][0]+=i;
+          col[j][1]++;
+        }
+      }
+    }
+
+    //Calculate Centroid
+    for(int i=0;i<n;i++)
+    {
+      if(!rowsum)
+      {
+        row[i][0]/=row[i][1];
+        rowsum+=row[i][0];
+        nrow++;
+      }
+      if(!colsum)
+      {
+        col[i][0]/=col[i][1];
+        colsum+=col[i][0];
+        ncol++;
+      }
+    }
+
+    X=rowsum/nrow*STEPS*dist;
+    Y=colsum/ncol*dist*STEPS;
+    Serial.println("The centroid is at ");
+    Serial.println(rowsum/nrow , colsum/ncol);
+    Serial.print(X);
+    Serial.print(" STEPS in x-direction and ");
+    Serial.print(Y); 
+    Serial.println(" STEPS in y-direction rotationsto be made");
+  }
+  PT_END(pt);
+}
+
+void loop()
+{
+  motor(&mtr);
+  sensor(&snr);
+}
+
